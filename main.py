604d0d5fef20ba6d34d90646b5f98f61707b4142
Reemma Puredath <reemma.puredath@sainsburys.co.uk>
2021-02-24 10:56:45 +0000

    remove error

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from typing import List
 
 class Friend:
     nicknames = [] # type: List
-    age = 0 # type: int
 
     def __init__(self, name: str) -> None:
         self.name = name
@@ -10,15 +9,11 @@ class Friend:
     def add_nickname(self, nickname: str) -> None:
         self.nicknames.append(nickname)
 
-    def celebrate_birthday(self) -> None:
-        self.age += 1
-
 def main() -> List:
     Reemma = Friend("Reemma")
     Reemma.add_nickname("Rem")
     Reemma.add_nickname("Rema")
-    Reemma.celebrate_birthday()
-    return Reemma.age
+    return Reemma.nicknames
 
 if __name__ == "__main__":
     res = main()
